f4f5c3fa9455d5c9d622ee8564134bc765e47484
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-07-05 15:53:39 +0200

    ADD if mails

--- js/script.js
+++ js/script.js
@@ -4,7 +4,21 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            mail: [
 
+            ]
         }
+    },
+
+    created() {
+       for (let index = 0; index < 10; index++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) => {
+            const result = response.data;
+            this.mail.push(result.response)
+        }); 
+       }
+       console.log(this.mail)  
     }
+    
 }).mount('#app')
